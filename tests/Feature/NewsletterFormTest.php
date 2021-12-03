018f1b9830b8e1a4c9b5b5288a56d99f34be3b47
Daniel Ang <daniel@sysanalyst.eu>
2021-12-03 22:50:50 +0100

    tests: Teste para o bug do email

--- tests/Feature/NewsletterFormTest.php
+++ tests/Feature/NewsletterFormTest.php
@@ -21,3 +21,21 @@ it('registers a new subscriber', function () {
         ->name->toBe('Virgão')
         ->email->toBe('virgu@front-end-masters.com');
 });
+
+it('validates wrong inputs', function (array $input, string $message) {
+    livewire(NewsletterForm::class)
+        ->set('name', $input['name'])
+        ->set('email', $input['email'])
+        ->call('submit')
+        ->assertSee($message);
+})->with([
+    [ ['name' => '', 'email' => ''], 'Pô, esqueceu de escrever o name'],
+    [ ['name' => 'da', 'email' => ''], 'Aqui não paga por letra enviada! Manda no mínimo 3'],
+    [ ['name' => 'virgão jobs', 'email' => ''], 'Pô, esqueceu de escrever o email'],
+    [ ['name' => 'virgão jobs', 'email' => 'vig@'], 'Bebeu todas? Email é tipo voce@voce.com.br']
+]);
+
+    /*
+
+]);
+*/
